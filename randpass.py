a8c7d8ee8b5378e0f94faf7f84683be9fde5026f
opensourcenikola <110063322+opensourcenikola@users.noreply.github.com>
2022-07-26 22:31:47 +0200

    Add files via upload

---
+++ randpass.py
@@ -0,0 +1,19 @@
+import random
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "0123456789"
+symbol = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/"
+ans = upper_case + lower_case + num + symbol
+
+length = 20
+password = "".join(random.sample(ans,length))
+print(password)
+
+# You can save your passwords here:        
+# 
+# 
+# 
+# 
+# 
+# 
+# 
